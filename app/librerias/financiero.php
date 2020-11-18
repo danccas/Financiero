e49c772c1dfc4c4f58525fecfe6b7b10dde36a0c
Diego Anccas <diego.anccas@gmail.com>
2020-11-17 19:39:53 -0500

    mensaje

--- app/librerias/financiero.php
+++ app/librerias/financiero.php
@@ -1,5 +1,7 @@
 <?php
 function encontrarle_una_categoria($db, $rotulo) {
+  $rotulo = str_replace('-', '', $rotulo);
+  $rotulo = str_replace('+', '', $rotulo);
   $rp = $db->get("SELECT categoria_id, descripcion, MATCH (descripcion) AGAINST ('" . $rotulo . "' IN BOOLEAN MODE) as score
     FROM movimiento
     WHERE categoria_id IS NOT NULL AND categoria_confirmacion = 1
